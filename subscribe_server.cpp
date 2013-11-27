b3b504578eacf5b2c94e9e91057d78b05c6f221a
Tom Schönfelder <tom@walle.int.uni-rostock.de>
2013-11-27 14:35:49 +0100

    changed name

---
+++ subscribe_server.cpp
@@ -0,0 +1,249 @@
+#include <websocketpp/config/asio_no_tls.hpp>
+
+#include <websocketpp/server.hpp>
+
+#include <iostream>
+
+/*#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition_variable.hpp>*/
+#include <websocketpp/common/thread.hpp>
+
+#include <chrono>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+using websocketpp::lib::thread;
+using websocketpp::lib::mutex;
+using websocketpp::lib::unique_lock;
+using websocketpp::lib::condition_variable;
+
+/* on_open insert connection_hdl into channel
+ * on_close remove connection_hdl from channel
+ * on_message queue send to all channels
+ */
+
+enum action_type 
+{
+    SUBSCRIBE,
+    UNSUBSCRIBE,
+    MESSAGE
+};
+
+enum subscribe_topic
+{
+	SOURCES,
+	GLOBAL,
+	REFERENCE,
+	MASTERLEVEL,
+	SOURCELEVEL,
+	LOUDSPEAKERLEVEL
+};
+
+struct action 
+{
+    action(action_type t, connection_hdl h) : type(t), hdl(h) {}
+    action(action_type t, server::message_ptr m) : type(t), msg(m) {}
+
+    action_type type;
+	
+    //if type is SUBSCRIBE, this defines the topic to which the client wants to subscribe
+	subscribe_topic topic;  
+	
+    websocketpp::connection_hdl hdl;
+    server::message_ptr msg;
+};
+
+std::string scenefile;
+server::message_ptr initial_scene;
+
+class SubscribeServer 
+{
+public:
+    SubscribeServer() 
+    {
+        // Initialize Asio Transport
+        _m_server.init_asio();
+
+        // Register handler callbacks
+        _m_server.set_open_handler(bind(&SubscribeServer::on_open,this,::_1));
+        _m_server.set_close_handler(bind(&SubscribeServer::on_close,
+            this,::_1));
+        _m_server.set_message_handler(bind(&SubscribeServer::on_message,
+            this,::_1,::_2));
+    }
+
+    /** 
+    *
+    *
+    **/
+    void run(uint16_t port) 
+    {
+        // listen on specified port
+        _m_server.listen(port);
+
+        // Start the server accept loop
+	    _m_server.start_accept();
+
+	    // Start the ASIO io_service run loop
+        try 
+        {
+            _m_server.run();
+        }
+
+        catch (const std::exception & e) 
+        {
+            std::cout << e.what() << std::endl;
+        } 
+        catch (websocketpp::lib::error_code e)
+        {
+            std::cout << e.message() << std::endl;
+        }
+        catch (...) 
+        {
+            std::cout << "other exception" << std::endl;
+        }
+    }
+
+    void on_open(connection_hdl hdl) 
+    {
+        unique_lock<mutex> lock(_m_action_lock);
+        //std::cout << "on_open" << std::endl;
+        _m_actions.push(action(SUBSCRIBE,hdl));
+        lock.unlock();
+        _m_action_cond.notify_one();
+    }
+
+    void on_close(connection_hdl hdl) 
+    {
+        unique_lock<mutex> lock(_m_action_lock);
+        //std::cout << "on_close" << std::endl;
+        _m_actions.push(action(UNSUBSCRIBE,hdl));
+        lock.unlock();
+        _m_action_cond.notify_one();
+    }
+
+    void on_message(connection_hdl hdl, server::message_ptr msg) 
+    {
+        // queue message up for sending by processing thread
+        unique_lock<mutex> lock(_m_action_lock);
+        //std::cout << "on_message" << std::endl;
+        _m_actions.push(action(MESSAGE,msg));
+        lock.unlock();
+        _m_action_cond.notify_one();
+    }
+
+    void process_messages()
+    {
+        while(1) 
+        {
+            unique_lock<mutex> lock(_m_action_lock);
+
+            while(_m_actions.empty()) _m_action_cond.wait(lock);
+
+            action a = _m_actions.front();
+            _m_actions.pop();
+
+            lock.unlock();
+
+            if (a.type == SUBSCRIBE)  //add new Client and send it the current scene
+            { 
+                unique_lock<mutex> lock(_m_connection_lock);
+                _m_connections.insert(a.hdl);
+				
+				//send current scene to new Subscriber
+				_m_server.send(a.hdl,initial_scene);
+				
+				lock.unlock();
+				
+            } 
+            else if (a.type == UNSUBSCRIBE) //remove client from list
+            { 
+                unique_lock<mutex> lock(_m_connection_lock);
+                _m_connections.erase(a.hdl);
+				lock.unlock();
+            } 
+            else if (a.type == MESSAGE) //store changes to scene in queue
+            { 
+				unique_lock<mutex> lock(_m_msg_lock);
+				_m_msgs.push(a.msg);
+				lock.unlock();
+			} 
+            else 
+            {
+                // undefined.
+            }
+        }
+    }
+	
+	void update_scene() 
+    {
+		while (1)
+        {
+			server::message_ptr messageToSend;
+			//std::string teststring = "This is a Test.";
+			//messageToSend->set_payload(teststring);
+			//wait for 100 ms
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			
+			unique_lock<mutex> lock(_m_connection_lock);
+			
+		
+			con_list::iterator it;
+			for (it = _m_connections.begin(); it != _m_connections.end(); ++it) 
+            {
+                _m_server.send(*it,messageToSend);
+            }
+		}
+	}
+private:
+    typedef std::set<connection_hdl,std::owner_less<connection_hdl>> con_list;
+
+    server _m_server;
+    con_list _m_connections;
+    std::queue<action> _m_actions;
+	std::queue<server::message_ptr> _m_msgs;
+
+    mutex _m_action_lock;
+    mutex _m_connection_lock;
+	mutex _m_msg_lock;
+    condition_variable _m_action_cond;
+};
+
+int main()
+{
+	try
+    {
+	::scenefile = "enviroment.json";
+	
+	SubscribeServer server;
+
+	// Start a thread to run the processing loop
+	thread processing_t(bind(&SubscribeServer::process_messages,&server));
+		
+	// Start a thread to run the update loop
+	thread update_t(bind(&SubscribeServer::update_scene,&server));
+
+	// Run the asio loop with the main thread
+    std::cout << "TestServer running..." << std::endl;
+	server.run(9002);
+  
+
+	processing_t.join();
+	update_t.join();
+
+	} 
+    catch  (std::exception & e) 
+    {
+       std::cout << e.what() << std::endl;
+    }
+
+}
+
+// Settings for Vim (http://www.vim.org/), please do not remove:
+// vim:softtabstop=2:shiftwidth=2:expandtab:textwidth=80:cindent
